ed4cad37230fc3f4299a6a38bda4c3f7f35efd20
Eunsik Lee <goldskull95@inha.edu>
2019-07-31 15:45:18 +0900

    190731

--- CF/1197c.cpp
+++ CF/1197c.cpp
@@ -5,19 +5,14 @@ int N, K, arr[300000], diff[300000];
 
 int main()
 {
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
 	cin >> N >> K;
 	for (int i = 0; i < N; i++) cin >> arr[i];
-
-	int minv, maxv;
-	minv = maxv = arr[0];
-	for (int i = 1; i < N; i++) {
-		minv = min(minv, arr[i]);
-		maxv = max(maxv, arr[i]);
-		diff[i - 1] = arr[i] - arr[i - 1];
-	}
+	for (int i = 1; i < N; i++) diff[i - 1] = arr[i] - arr[i - 1];
 
 	sort(diff, diff + N - 1, greater<int>());
 	int sum = 0;
 	for (int i = 0; i < K - 1; i++) sum += diff[i];
-	cout << maxv - minv - sum << "\n";
+	cout << arr[N - 1] - arr[0] - sum << "\n";
 }
